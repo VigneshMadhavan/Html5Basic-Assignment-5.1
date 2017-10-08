2258bff83f49ced1d49a05dc20e3ab579b300950
VigneshMadhavan <vigneshmadhavan@yahoo.co.in>
2017-10-08 21:58:48 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
+//function to calculate Area for the circle
 function areaOfCircle(dia){
-	
+	//check for the number and not null
 	if(isNaN(dia) || dia==null){
+		//if null or not a number then ask for the dia again
 		askDia("Plese enter the diameter of circle in number");
 	}else{
+		//if it is number calculate area
 		var area=(Math.PI*(dia/2)*(dia/2));
 		document.write("Area of the circle for diameter "+dia+" is "+area);
 	}
@@ -11,7 +14,9 @@ function areaOfCircle(dia){
 
 function askDia(msg){
 	var dia =prompt(msg,0);
+	//pass the dia to calculate Area
 	areaOfCircle(dia);
 }
+//call the function to get the diameter
 askDia("Plese enter the diameter of circle");
 
